db47aaad6398938385866636dbe7af15bda2ab0d
Moreira <moreira_micael@hotmail.com>
2017-12-05 15:52:45 +0000

    1
    1

--- src/projecto/CP2/Avaria.java
+++ src/projecto/CP2/Avaria.java
@@ -1,4 +1,13 @@
 package projecto.CP2;
 
+import java.util.Calendar;
+
 public class Avaria {
+    private int id;
+    private Calendar dataRegisto;
+    private Equipamento equipamentoAssociado;
+    private String descricaoReparacao;
+    private Funcionario funcionarioRegistou;
+
+
 }
